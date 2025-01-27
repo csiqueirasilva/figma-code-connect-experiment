549d12f66fddd6e6f56a8f5b2d57cf21cad93c55
Caio Siqueira <csiqueirasilva@gmail.com>
2025-01-27 00:25:35 -0300

    Updates

---
+++ assets/TypographyP-ZI31sJaM.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DkCuz2Fy.js";function o({children:t}){return r.jsx("p",{className:"leading-7 [&:not(:first-child)]:mt-6",children:t})}export{o as T};
--- assets/conclusao-B_yIHRun.js
+++ assets/conclusao-B_yIHRun.js
@@ -1,4 +1,4 @@
-import{j as o}from"./index-Dx9HIC-h.js";function n(a){const e={li:"li",ul:"ul",...a.components};return o.jsxs(e.ul,{children:[`
+import{j as o}from"./index-DkCuz2Fy.js";function n(a){const e={li:"li",ul:"ul",...a.components};return o.jsxs(e.ul,{children:[`
 `,o.jsx(e.li,{children:"Essa página foi desenvolvida inicialmente com shadcn, react 19 e vite. Ao chegar ao ponto de incluir o componente do sds houve vários conflitos pois ele estava em react 18 com outra versão do vite. Diversos ajustes tiveram de ser feitos (css important via tailwind)."}),`
 `,o.jsx(e.li,{children:"Foi gerado um script chamado merge-dependencies.js com IA para pegar as dependencias do SDS e jogar em cima desse repositório. Por conta disso, o package.json está completamente poluído."}),`
 `,o.jsx(e.li,{children:"No geral a ferramenta funciona bem, mas é preciso estar corretamente estruturado para que se ganhe o benefício de gerar telas rapidamente a partir de componentes previamente cadastrados."}),`
