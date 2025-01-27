bc167581c58bf1f62a8c0fcc1d4b6d1ffc5c2a40
Caio Siqueira <csiqueirasilva@gmail.com>
2025-01-27 00:25:21 -0300

    Updates

---
+++ assets/TypographyP-1GTi8TJG.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Dx9HIC-h.js";function o({children:t}){return r.jsx("p",{className:"leading-7 [&:not(:first-child)]:mt-6",children:t})}export{o as T};
--- assets/TypographyP-CeK7FfoT.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DpnAxheF.js";function o({children:t}){return r.jsx("p",{className:"leading-7 [&:not(:first-child)]:mt-6",children:t})}export{o as T};
